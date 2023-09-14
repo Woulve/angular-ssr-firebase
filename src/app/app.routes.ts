def287a739ae2a1b4ec3bcf4bce4ebf541cfdc4e
Elias Cecetka <elias_2000@web.de>
2023-09-14 21:26:37 +0200

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./test/test.component').then((m) => m.TestComponent),
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const firebaseConfig = {
+  apiKey: 'AIzaSyC8sPhtlHr8y3Fx17TzsffnVF4Zih0fSz4',
+  authDomain: 'angular-ssr-firebase-7dfdd.firebaseapp.com',
+  projectId: 'angular-ssr-firebase-7dfdd',
+  storageBucket: 'angular-ssr-firebase-7dfdd.appspot.com',
+  messagingSenderId: '954486474849',
+  appId: '1:954486474849:web:9fbc967056cdeaab0cc220',
+  measurementId: 'G-TMQG5G7E1V',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
